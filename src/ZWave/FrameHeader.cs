545a122ff6402f19db924a842da1e3ec42652478
David Federman <david.federman@outlook.com>
2021-12-11 07:15:50 -0800

    Started implementation of serial api parser

---
+++ src/ZWave/FrameHeader.cs
@@ -0,0 +1,27 @@
+﻿namespace ZWave;
+
+internal static class FrameHeader
+{
+    /// <summary>
+    /// The SOF (Start of Frame) denotes the start of a data frame, which contains the Serial API
+    /// command including parameters for the command in question.
+    /// </summary>
+    public const byte SOF = 0x01;
+
+    /// <summary>
+    /// The ACK frame indicates that the receiving end received a valid Data frame.
+    /// </summary>
+    public const byte ACK = 0x06;
+
+    /// <summary>
+    /// The NAK frame indicates that the receiving end received a Data frame with errors.
+    /// </summary>
+    public const byte NAK = 0x15;
+
+    /// <summary>
+    /// The CAN frame indicates that the receiving end discarded an otherwise valid Data frame.
+    /// The CAN frame is used to resolve race conditions, where both ends send a Data frame and
+    /// subsequently expects an ACK frame from the other end.
+    /// </summary>
+    public const byte CAN = 0x18;
+}
---
+++ src/ZWave/ZWaveSerialApiParser.cs
@@ -0,0 +1,59 @@
+﻿using System.Buffers;
+
+namespace ZWave;
+
+public sealed class ZWaveSerialApiParser
+{
+    public void TryParseData(ref ReadOnlySequence<byte> buffer)
+    {
+        if (buffer.IsEmpty)
+        {
+            // No data?
+            return;
+        }
+
+        var reader = new SequenceReader<byte>(buffer);
+
+        if (!reader.TryRead(out byte frameHeader))
+        {
+            // No data?
+            return;
+        }
+
+        switch (frameHeader)
+        {
+            case FrameHeader.ACK:
+            {
+                // TODO:
+                break;
+            }
+            case FrameHeader.NAK:
+            {
+                // TODO:
+                break;
+            }
+            case FrameHeader.CAN:
+            {
+                // TODO:
+                break;
+            }
+            case FrameHeader.SOF:
+            {
+                // TODO:
+                break;
+            }
+            default:
+            {
+                TryHandleDataFrame(reader);
+                break;
+            }
+        }
+    }
+
+    private void TryHandleDataFrame(SequenceReader<byte> reader)
+    {
+        // We've alread read the SOF
+
+        // TODO
+    }
+}
