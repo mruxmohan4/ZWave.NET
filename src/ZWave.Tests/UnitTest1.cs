9e8ab1971a22ee57851bbb64c29c4711c0307d84
David Federman <david.federman@outlook.com>
2021-12-10 21:18:14 -0800

    Add project scaffolding

---
+++ src/ZWave.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZWave.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
