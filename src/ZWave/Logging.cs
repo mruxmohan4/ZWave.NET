2cadcbd141d51896537ad9b6306ff9e0374da35a
David Federman <david.federman@outlook.com>
2022-05-11 21:43:55 -0700

    Renumber logging event ids

--- src/ZWave/Logging.cs
+++ src/ZWave/Logging.cs
@@ -6,86 +6,90 @@ namespace ZWave;
 
 internal static partial class Logging
 {
+    /* SerialApi: 100-199 */
+
     [LoggerMessage(
-        EventId = 0,
+        EventId = 100,
         Level = LogLevel.Trace,
         Message = "ZWave Serial port `{portName}` opened")]
     public static partial void LogSerialApiPortOpened(this ILogger logger, string portName);
 
     [LoggerMessage(
-        EventId = 1,
+        EventId = 101,
         Level = LogLevel.Trace,
         Message = "ZWave Serial port `{portName}` closed")]
     public static partial void LogSerialApiPortClosed(this ILogger logger, string portName);
 
     [LoggerMessage(
-        EventId = 2,
+        EventId = 103,
         Level = LogLevel.Trace,
         Message = "Skipped {numBytes} bytes of invalid data from the Serial port")]
     public static partial void LogSerialApiSkippedBytes(this ILogger logger, long numBytes);
 
     [LoggerMessage(
-        EventId = 3,
+        EventId = 104,
         Level = LogLevel.Trace,
         Message = "Received partial Serial API data frame with length {length}")]
     public static partial void LogSerialApiPartialDataFrameReceived(this ILogger logger, long length);
 
     [LoggerMessage(
-        EventId = 4,
+        EventId = 105,
         Level = LogLevel.Trace,
         Message = "Received Serial API frame: {frame}")]
     public static partial void LogSerialApiFrameReceived(this ILogger logger, Frame frame);
 
     [LoggerMessage(
-        EventId = 5,
+        EventId = 106,
         Level = LogLevel.Trace,
         Message = "Received invalid Serial API data frame: {frame}")]
     public static partial void LogSerialApiInvalidDataFrame(this ILogger logger, DataFrame frame);
 
     [LoggerMessage(
-        EventId = 6,
+        EventId = 107,
         Level = LogLevel.Trace,
         Message = "Sent Serial API frame: {frame}")]
     public static partial void LogSerialApiFrameSent(this ILogger logger, Frame frame);
 
     [LoggerMessage(
-        EventId = 7,
+        EventId = 108,
         Level = LogLevel.Trace,
         Message = "Sent Serial API data frame: {frame}")]
     public static partial void LogSerialApiDataFrameSent(this ILogger logger, DataFrame frame);
 
     [LoggerMessage(
-        EventId = 8,
+        EventId = 109,
         Level = LogLevel.Trace,
         Message = "Received Serial API data frame with unknown type `{dataFrameType}`")]
     public static partial void LogSerialApiDataFrameUnknownType(this ILogger logger, DataFrameType dataFrameType);
 
+    /* Driver: 200-299 */
+
     [LoggerMessage(
-        EventId = 9,
+        EventId = 200,
         Level = LogLevel.Information,
         Message = "Driver initialization sequence starting")]
     public static partial void LogDriverInitializing(this ILogger logger);
 
     [LoggerMessage(
-        EventId = 10,
+        EventId = 201,
         Level = LogLevel.Information,
         Message = "Performing soft reset")]
     public static partial void LogSoftReset(this ILogger logger);
 
     [LoggerMessage(
-        EventId = 11,
+        EventId = 202,
         Level = LogLevel.Information,
         Message = "Driver initialization sequence complete")]
     public static partial void LogDriverInitialized(this ILogger logger);
 
     [LoggerMessage(
-        EventId = 12,
+        EventId = 203,
         Level = LogLevel.Debug,
         Message = "Identifying controller")]
     public static partial void LogControllerIdentifying(this ILogger logger);
 
     [LoggerMessage(
-        EventId = 13,
+        EventId = 204,
         Level = LogLevel.Information,
         Message = "Controller identity:\n" +
         "Home ID = {homeId}\n" +
@@ -93,7 +97,7 @@ internal static partial class Logging
     public static partial void LogControllerIdentity(this ILogger logger, uint homeId, byte nodeId);
 
     [LoggerMessage(
-        EventId = 14,
+        EventId = 205,
         Level = LogLevel.Information,
         Message = "Serial API capabilities:\n" +
         "Serial API Version = {serialApiVersion}\n" +
@@ -110,7 +114,7 @@ internal static partial class Logging
         string supportedCommands);
 
     [LoggerMessage(
-        EventId = 15,
+        EventId = 206,
         Level = LogLevel.Information,
         Message = "Controller library:\n" +
         "Library version = {libraryVersion}\n" +
@@ -118,31 +122,31 @@ internal static partial class Logging
     public static partial void LogControllerLibraryVersion(this ILogger logger, string libraryVersion, LibraryType libraryType);
 
     [LoggerMessage(
-        EventId = 16,
+        EventId = 207,
         Level = LogLevel.Information,
         Message = "Controller capabilities: {controllerCapabilities}")]
     public static partial void LogControllerCapabilities(this ILogger logger, ControllerCapabilities controllerCapabilities);
 
     [LoggerMessage(
-        EventId = 17,
+        EventId = 208,
         Level = LogLevel.Information,
         Message = "Supported Serial API Setup subcommands: {supportedSubcommands}")]
     public static partial void LogControllerSupportedSerialApiSetupSubcommands(this ILogger logger, string supportedSubcommands);
 
     [LoggerMessage(
-        EventId = 18,
+        EventId = 209,
         Level = LogLevel.Debug,
         Message = "Enabling TX status report success: {success}")]
     public static partial void LogEnableTxStatusReport(this ILogger logger, bool success);
 
     [LoggerMessage(
-        EventId = 19,
+        EventId = 210,
         Level = LogLevel.Debug,
         Message = "SUC Node Id: {sucNodeId}")]
     public static partial void LogControllerSucNodeId(this ILogger logger, byte sucNodeId);
 
     [LoggerMessage(
-        EventId = 20,
+        EventId = 211,
         Level = LogLevel.Information,
         Message = "Init data:\n" +
         "API Version = {apiVersion}\n" +
