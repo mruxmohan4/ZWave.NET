cba795f3c81ad05c15c934614b6ea49b0abc49ee
David Federman <david.federman@outlook.com>
2022-07-13 22:22:02 -0700

    Add a base comopnent type for CCs (#48)

---
+++ src/ZWave.Server/CommandClasses/CommandClassComponentBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ZWave.Server.CommandClasses;
+
+public class CommandClassComponentBase : ComponentBase
+{
+    protected string? ErrorMessage { get; private set; }
+
+    protected Exception? ErrorException { get; private set; }
+
+    protected async Task RunSafelyAsync(Func<Task> action, string methodName)
+    {
+        ClearError();
+        try
+        {
+            await action();
+        }
+        catch (Exception ex)
+        {
+            ErrorMessage = $"Error calling {methodName}";
+            ErrorException = ex;
+        }
+    }
+
+    private void ClearError()
+    {
+        ErrorMessage = null;
+        ErrorException = null;
+    }
+}
